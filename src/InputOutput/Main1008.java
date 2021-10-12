873658a160d2d4c17d262b1ef549c4655a60c0db
leeumji <umzlee1987@gmail.com>
2021-10-13 04:12:16 +0900

    1008 - 사칙연산 (/)

---
+++ src/InputOutput/Main1008.java
@@ -0,0 +1,14 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class Main1008 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        double a = scr.nextDouble();
+        double b = scr.nextDouble();
+
+        System.out.println(a/b);
+
+    }
+}
