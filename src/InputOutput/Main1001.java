d63c4b7a25513f958c812dbee106dbf0c7d448c8
leeumji <umzlee1987@gmail.com>
2021-10-13 04:01:15 +0900

    1000 - 사칙연산 (-)

---
+++ src/InputOutput/Main1001.java
@@ -0,0 +1,14 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class Main1001 {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+
+        System.out.println(a-b);
+    }
+}
