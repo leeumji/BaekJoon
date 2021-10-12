5842e91516b9a581b916f23d52dc4cb688030132
leeumji <umzlee1987@gmail.com>
2021-10-13 04:26:33 +0900

    10869 - 사칙연산

---
+++ src/InputOutput/Main10869.java
@@ -0,0 +1,18 @@
+package InputOutput;
+
+import java.util.Scanner;
+
+public class Main10869 {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
