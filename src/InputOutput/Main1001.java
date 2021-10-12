263445faa746fc0c0633a4f301ad7a64af890381
leeumji <umzlee1987@gmail.com>
2021-10-13 04:17:31 +0900

    1001 - 사칙연산 (-)

--- src/InputOutput/Main1001.java
+++ src/InputOutput/Main1001.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main1001 {
     public static void main(String[] args) {
-
         Scanner scr = new Scanner(System.in);
         int a = scr.nextInt();
         int b = scr.nextInt();
