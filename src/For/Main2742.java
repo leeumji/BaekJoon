ac6ad468f77b3bd3c2056143c77d8240f859155b
leeumji <umzlee1987@gmail.com>
2021-10-20 19:36:41 +0900

    2742 - 기찍 N

---
+++ src/For/Main2742.java
@@ -0,0 +1,15 @@
+package For;
+
+import java.util.Scanner;
+
+public class Main2742 {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+
+        for (int i = a; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+}
