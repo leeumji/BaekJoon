38ecd72539f4bc0feae619d6f343b18a3e7337ff
leeumji <umzlee1987@gmail.com>
2021-10-20 19:21:08 +0900

    15552 - 빠른 A+B

---
+++ src/For/Main2741.java
@@ -0,0 +1,16 @@
+package For;
+
+import java.util.Scanner;
+
+public class Main2741 {
+    public static void main(String[] args) {
+
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+
+            System.out.println();
+        }
+    }
+}
